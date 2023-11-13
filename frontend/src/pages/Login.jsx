2744f2764b2bd709d66cf60c8108f64f2366ca8e
Antoni Walach <walachantoni@gmail.com>
2023-11-13 23:16:56 +0100

    Login delivery

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,55 +1,98 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Validation from "../utils/loginValidation";
-import axios from "axios"
-import '../assets/css/customLogin.css';
-
+import axios from "axios";
+import "../assets/css/customLogin.css";
 
 function Login() {
   const [values, setValues] = useState({
     email: "",
     password: "",
+    userType: "user",
   });
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [errors, setErrors] = useState({});
 
   const handleInput = (event) => {
     setValues((prev) => ({
       ...prev,
-      [event.target.name]: [event.target.value],
+      [event.target.name]: event.target.value,
     }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setErrors(Validation(values));
-    if(errors.email === "" && errors.password === "") {
-        axios.post("http://localhost:8081/login", values)
-        .then(res => {
-            if(res.data === "Success"){
-                navigate("/home")
-            } else {
-                alert("No record existed.")
-            }
+    if (errors.email === "" && errors.password === "") {
+      axios
+        .post("http://localhost:8081/login", values)
+        .then((res) => {
+          if (res.data === "Success client") {
+            navigate("/home");
+          } else if (res.data === "Success delivery") {
+            navigate("/delivery");
+          } else {
+            //console.log(res.data)
+            alert("No record existed.");
+          }
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
     }
   };
 
   return (
     <div>
-       <div className="px-3 pt-2">
-          <Link to="/">
-            <div class="arrow arrow-left"></div>
-          </Link>
+      <div className="px-3 pt-2">
+        <Link to="/">
+          <div class="arrow arrow-left"></div>
+        </Link>
       </div>
+
       <div className="d-flex justify-content-center align-items-center vh-100">
         <div className="bg-white p-3 rounded w-25 custom-card-login-signup">
-          <h2>Log In</h2>
+          <div className="d-flex justify-content-between align-items-center vh-20">
+            <h2 className="mb-3">Log In</h2>
+            <div
+              className="btn-group"
+              role="group"
+              aria-label="Basic radio toggle button group"
+            >
+              <input
+                type="radio"
+                className="btn-check custom-button-radio"
+                name="userType"
+                id="btnradio1"
+                defaultChecked
+                onChange={() => {
+                  handleInput({
+                    target: { name: "userType", value: "user" },
+                  });
+                }}
+              />
+              <label className="btn custom-radio-outline" htmlFor="btnradio1">
+                Client
+              </label>
+
+              <input
+                type="radio"
+                className="btn-check custom-button-radio"
+                name="userType"
+                id="btnradio2"
+                autoComplete="off"
+                onChange={() => {
+                  handleInput({
+                    target: { name: "userType", value: "delivery" },
+                  });
+                }}
+              />
+              <label className="btn custom-radio-outline" htmlFor="btnradio2">
+                Delivery
+              </label>
+            </div>
+          </div>
           <form action="" onSubmit={handleSubmit}>
-            
             <div className="mb-3">
               <label htmlFor="email">
                 <strong>Email</strong>
@@ -67,7 +110,9 @@ function Login() {
             </div>
 
             <div className="mb-3">
-              <label htmlFor="password"><strong>Password</strong></label>
+              <label htmlFor="password">
+                <strong>Password</strong>
+              </label>
               <input
                 type="password"
                 placeholder="Enter Password"
@@ -80,7 +125,10 @@ function Login() {
               )}
             </div>
 
-            <button type="submit" className="btn btn-success w-100 custom-button-log-in mb-3">
+            <button
+              type="submit"
+              className="btn btn-success w-100 custom-button-log-in mb-3"
+            >
               <strong>Log in</strong>
             </button>
             <Link
