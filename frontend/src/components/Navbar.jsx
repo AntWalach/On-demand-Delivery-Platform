88d095a7f2a89d768e5900fcd559ead8a6400fb2
bszarlowicz <b,szarlowicz@gmail.com>
2023-11-13 14:58:48 +0100

    className mistake

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,34 +4,34 @@ import '../assets/css/customNavbar.css';
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
           <img src={QuickPackLogo} className="App-logo" alt="logo" width="115" height="70"/>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link custom-navbar-text-color" href="#">My orders</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link custom-navbar-text-color" href="#">My orders</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle  custom-navbar-text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle  custom-navbar-text-color" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 My profile
               </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Wallet</a></li>
-                <li><a class="dropdown-item" href="#">Account Settings</a></li>
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="#">History of orders</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Wallet</a></li>
+                <li><a className="dropdown-item" href="#">Account Settings</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">History of orders</a></li>
               </ul>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search for orders..." aria-label="Search"/>
-            <button class="btn btn-outline-success custom-button-home" type="submit">Search</button>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search for orders..." aria-label="Search"/>
+            <button className="btn btn-outline-success custom-button-home" type="submit">Search</button>
           </form>
         </div>
       </div>
