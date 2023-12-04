7ccd4f006e57304e5a3779a58433f2fb4baee004
bszarlowicz <b.szarlowicz@gmail.com>
2023-12-04 12:01:06 +0100

    delivery background fix v2

--- frontend/src/components/Layouts/NavbarDelivery.jsx
+++ frontend/src/components/Layouts/NavbarDelivery.jsx
@@ -56,6 +56,14 @@ function NavbarDelivery() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+              <a
+                className={`${customNavbarDelivery.customNavbarTextColor} nav-link`}
+                href="/delivery"
+              >
+                My orders
+              </a>
+            </li>
             <li className="nav-item">
               <a
                 className={`${customNavbarDelivery.customNavbarTextColor} nav-link`}
