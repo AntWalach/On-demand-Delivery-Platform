defcd9711526e820a2ebfa18d79cff7bb1c4e702
maciek102 <walczakmaciek@wp.pl>
2023-12-29 17:10:40 +0100

    client wallet frontend

--- frontend/src/pages/Client/ClientWallet.jsx
+++ frontend/src/pages/Client/ClientWallet.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../../components/Layouts/Navbar";
+import customWallet from "../../assets/css/Wallet.module.css";
 
 function ClientWallet() {
   axios.defaults.withCredentials = true;
@@ -10,6 +11,7 @@ function ClientWallet() {
   const [message, setMessage] = useState("");
   const [user, setUser] = useState("");
   const navigate = useNavigate();
+  const walletBalance = 0;
 
   useEffect(() => {
     axios
@@ -27,10 +29,54 @@ function ClientWallet() {
       })
       .catch((err) => console.log(err));
   }, [navigate]);
+
   return (
     <div>
       <Navbar />
-      <div>ClientWallet</div>
+      <div className="row">
+        <div className="col-md-12 text-center mt-5">
+          <h1 className={`${customWallet.customTextColorHeader} display-6`}>
+            Wallet
+          </h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-12 text-center mt-5">
+          <h1 className={`${customWallet.customTextColorHeader} display-6`}>
+            My balance : ${walletBalance}
+          </h1>
+        </div>
+      </div>
+
+      <div className="row my-5"></div>
+
+      <div className="row mt-5">
+        <div class="input-group" style={{ width: "30%", margin: "0 auto" }}>
+          <span class="input-group-text">Top up your wallet</span>
+          <div class="form-floating">
+            <input
+              type="text"
+              class="form-control"
+              id="floatingInputGroup1"
+              placeholder="Username"
+            />
+            <label for="floatingInputGroup1">Enter value</label>
+          </div>
+        </div>
+
+        <div className="text-center mt-5">
+          <button
+            type="submit"
+            className={`${customWallet.customButtonWallet} btn btn-lg`}
+            style={{
+              padding: "8px 70px",
+            }}
+          >
+            <strong>Submit</strong>
+          </button>
+        </div>
+      </div>
+      
     </div>
   );
 }
