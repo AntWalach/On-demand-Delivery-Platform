4632e566702bb06cdf0cc36e628a4e41c7408cea
maciek102 <walczakmaciek@wp.pl>
2023-12-29 18:25:44 +0100

    admin frontend

---
+++ frontend/src/pages/Admin/AdminClient.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavbarAdmin from "../../components/Layouts/NavbarAdmin";
+import "../../assets/css/Admin.module.css";
+import customAdmin from "../../assets/css/Admin.module.css";
+
+function AdminClient() {
+  return (
+    <div>
+      <NavbarAdmin />
+      AdminClient
+      </div>
+  )
+}
+
+export default AdminClient
