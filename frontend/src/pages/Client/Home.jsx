3a882d8881c7dbd2d25c5fb75229b71dd26747b5
Antoni Walach <walachantoni@gmail.com>
2024-01-01 18:05:39 +0100

    Option ZPL download

--- frontend/src/pages/Client/Home.jsx
+++ frontend/src/pages/Client/Home.jsx
@@ -17,6 +17,7 @@ import ZebraPrintWrapper from "zebra-browser-print-wrapper";
 
 function Home() {
   axios.defaults.withCredentials = true;
+
   const [values, setValues] = useState({
     //deliveryOption: "",
     packageOption: "Small",
@@ -33,6 +34,12 @@ function Home() {
   });
 
   const [errors, setErrors] = useState({});
+  const [auth, setAuth] = useState(false);
+  const [message, setMessage] = useState("");
+  const [user, setUser] = useState("");
+  const [labelImage, setLabelImage] = useState("");
+  const [isZPLVisible, setZPLVisible] = useState(false);
+  const navigate = useNavigate();
 
   const handleInput = (event) => {
     setValues((prev) => ({
@@ -66,12 +73,6 @@ function Home() {
     }
   };
 
-  const [auth, setAuth] = useState(false);
-  const [message, setMessage] = useState("");
-  const [user, setUser] = useState("");
-  const [labelImage, setLabelImage] = useState("");
-  const navigate = useNavigate();
-
   useEffect(() => {
     axios
       .get("http://localhost:8081")
@@ -89,36 +90,50 @@ function Home() {
       .catch((err) => console.log(err));
   }, [navigate]);
 
-
-
   function convertToZPLString(inputString) {
     const polishCharsMap = {
-      ą: 'a',
-      ć: 'c',
-      ę: 'e',
-      ł: 'l',
-      ń: 'n',
-      ó: 'o',
-      ś: 's',
-      ź: 'z',
-      ż: 'z',
-      Ą: 'A',
-      Ć: 'C',
-      Ę: 'E',
-      Ł: 'L',
-      Ń: 'N',
-      Ó: 'O',
-      Ś: 'S',
-      Ź: 'Z',
-      Ż: 'Z',
+      ą: "a",
+      ć: "c",
+      ę: "e",
+      ł: "l",
+      ń: "n",
+      ó: "o",
+      ś: "s",
+      ź: "z",
+      ż: "z",
+      Ą: "A",
+      Ć: "C",
+      Ę: "E",
+      Ł: "L",
+      Ń: "N",
+      Ó: "O",
+      Ś: "S",
+      Ź: "Z",
+      Ż: "Z",
     };
-  
-    return inputString.replace(/[ąćęłńóśźżĄĆĘŁŃÓŚŹŻ]/g, (match) => polishCharsMap[match] || match);
-  }
 
+    return inputString.replace(
+      /[ąćęłńóśźżĄĆĘŁŃÓŚŹŻ]/g,
+      (match) => polishCharsMap[match] || match
+    );
+  }
 
   const handleGenerateZPL = async () => {
     try {
+      const validationErrors = Validation(values);
+
+      if (
+        validationErrors.InputZipCode1 ||
+        validationErrors.InputZipCode2 ||
+        validationErrors.InputCity1 ||
+        validationErrors.InputCity2 ||
+        validationErrors.InputStreet1 ||
+        validationErrors.InputStreet2 ||
+        validationErrors.InputBuildingNumber1 ||
+        validationErrors.InputBuildingNumber2
+      ) {
+        return;
+      }
       const generatedZPL = `^XA
       ^FX Top section with logo, name, and address.
       ^CF0,30
@@ -156,8 +171,7 @@ function Home() {
       ^FO100,1060^FDREF2 BL4H8^FS
       ^CF0,40
       ^FO470,955^FDCA^FS
-      ^XZ
-      `;
+      ^XZ`;
 
       const response = await fetch(
         "http://api.labelary.com/v1/printers/8dpmm/labels/4x6/0/",
@@ -174,6 +188,7 @@ function Home() {
       if (response.ok) {
         const imageUrl = URL.createObjectURL(await response.blob());
         setLabelImage(imageUrl);
+        setZPLVisible(true);
       } else {
         const errorMessage = await response.text();
         console.error("Error generating label:", errorMessage);
@@ -183,184 +198,200 @@ function Home() {
     }
   };
 
+  const handleDownloadImage = () => {
+    const link = document.createElement("a");
+    link.href = labelImage;
+    link.download = "label_image.png";
+    link.click();
+  };
+
   return (
     <div>
       <Navbar />
-      <div className="row mt-5 mx-auto">
-        <div className="text-center">
-          <h2 className={`${customHome.customTextColorHeadings} display-4`}>
-            Delivery destination
-          </h2>
-        </div>
-        <div>
-          <form
-            className="w-60 mx-auto justify-content-center text-center"
-            action=""
-            onSubmit={handleSubmit}
-          >
-            {/* Delivery options components */}
-            <div className="row justify-content-center mt-4 w-75 mx-auto">
-              <div
-                className={`${customHome.customColWidth} col-3 mx-5 d-flex justify-content-center mb-3`}
-              >
-                <DeliveryOption
-                  icon={
-                    <House className={`${customHome.customIcon} m-auto mt-5`} />
-                  }
-                  title="Address"
-                  description="The courier will deliver the parcel directly to the address"
-                />
-              </div>
-
-              <div
-                className={`${customHome.customColWidth} col-3 mx-5 d-flex justify-content-center mb-3`}
-              >
-                <DeliveryOption
-                  icon={
-                    <PinAngle
-                      className={`${customHome.customIcon} m-auto mt-5`}
-                    />
-                  }
-                  title="Shipping point"
-                  description="The courier will deliver the parcel at the shipping point"
-                />
-              </div>
+      <div>
+        {isZPLVisible ? (
+          <div className="mt-4 text-center bg-white rounded">
+            <button onClick={() => setZPLVisible(false)}> Close </button>
+            <button onClick={handleDownloadImage}>Download</button>
+            <div>
+              <img src={labelImage} alt="Label" />
             </div>
-
-            <div className="row mt-5">
-              <div className="col-12 text-center">
-                <h2
-                  className={`${customHome.customTextColorHeadings} display-4`}
-                >
-                  Pack size
-                </h2>
-              </div>
+          </div>
+        ) : (
+          <div className="row mt-5 mx-auto">
+            <div className="text-center">
+              <h2 className={`${customHome.customTextColorHeadings} display-4`}>
+                Delivery destination
+              </h2>
             </div>
-
-            {/* Package options components */}
-            <div className="row justify-content-center mt-4 p-0 w-75 mx-auto">
-              <div
-                className={`${customHome.customColWidth} col-3 mx-4 d-flex justify-content-center mb-3`}
+            <div>
+              <form
+                className="w-60 mx-auto justify-content-center text-center"
+                action=""
+                onSubmit={handleSubmit}
               >
-                <PackageOption
-                  icon={
-                    <BoxFill
-                      className={`${customHome.customIcon} m-auto mt-5`}
+                {/* Delivery options components */}
+                <div className="row justify-content-center mt-4 w-75 mx-auto">
+                  <div
+                    className={`${customHome.customColWidth} col-3 mx-5 d-flex justify-content-center mb-3`}
+                  >
+                    <DeliveryOption
+                      icon={
+                        <House
+                          className={`${customHome.customIcon} m-auto mt-5`}
+                        />
+                      }
+                      title="Address"
+                      description="The courier will deliver the parcel directly to the address"
                     />
-                  }
-                  title="Small"
-                  sizeInfo="max. 10 x 40 x 65 cm"
-                  weightInfo="up to 10 kg"
-                  price="$15"
-                  value="Small"
-                  onChange={handleInput}
-                />
-              </div>
+                  </div>
 
-              <div
-                className={`${customHome.customColWidth} col-3 mx-4 d-flex justify-content-center mb-3`}
-              >
-                <PackageOption
-                  icon={
-                    <BoxSeamFill
-                      className={`${customHome.customIcon} m-auto mt-5`}
+                  <div
+                    className={`${customHome.customColWidth} col-3 mx-5 d-flex justify-content-center mb-3`}
+                  >
+                    <DeliveryOption
+                      icon={
+                        <PinAngle
+                          className={`${customHome.customIcon} m-auto mt-5`}
+                        />
+                      }
+                      title="Shipping point"
+                      description="The courier will deliver the parcel at the shipping point"
                     />
-                  }
-                  title="Medium"
-                  sizeInfo="max. 20 x 40 x 65 cm"
-                  weightInfo="up to 20 kg"
-                  price="$20"
-                  value="Medium"
-                  onChange={handleInput}
-                />
-              </div>
-
-              <div
-                className={`${customHome.customColWidth} col-3 mx-4 d-flex justify-content-center mb-3`}
-              >
-                <PackageOption
-                  icon={
-                    <Box2Fill
-                      className={`${customHome.customIcon} m-auto mt-5`}
-                    />
-                  }
-                  title="Large"
-                  sizeInfo="max. 45 x 40 x 65 cm"
-                  weightInfo="up to 30 kg"
-                  price="$25"
-                  value="Large"
-                  onChange={handleInput}
-                />
-              </div>
-            </div>
-
-            <div className="row mt-5">
-              <div className="col-12 text-center">
-                <h2
-                  className={`${customHome.customTextColorHeadings} display-4`}
-                >
-                  Shipping details
-                </h2>
-              </div>
-            </div>
-
-            <div className="row justify-content-center mt-3 mb-5">
-              <div className="container">
-                <div className="row justify-content-center">
-                  <AddressFormSection
-                    label="Sender"
-                    zipCodeId="InputZipCode1"
-                    cityId="InputCity1"
-                    streetId="InputStreet1"
-                    buildingNumberId="InputBuildingNumber1"
-                    apartmentNumberId="InputApartmentNumber1"
-                    zipCode={values.InputZipCode1}
-                    city={values.InputCity1}
-                    street={values.InputStreet1}
-                    buildingNumber={values.InputBuildingNumber1}
-                    apartmentNumber={values.InputApartmentNumber1}
-                    errors={errors}
-                    handleInput={handleInput}
-                  />
-
-                  <AddressFormSection
-                    label="Recipient"
-                    zipCodeId="InputZipCode2"
-                    cityId="InputCity2"
-                    streetId="InputStreet2"
-                    buildingNumberId="InputBuildingNumber2"
-                    apartmentNumberId="InputApartmentNumber2"
-                    zipCode={values.InputZipCode2}
-                    city={values.InputCity2}
-                    street={values.InputStreet2}
-                    buildingNumber={values.InputBuildingNumber2}
-                    apartmentNumber={values.InputApartmentNumber2}
-                    errors={errors}
-                    handleInput={handleInput}
-                  />
+                  </div>
                 </div>
-              </div>
 
-              <div className="text-center mt-2">
-                <button
-                  type="submit"
-                  className={`${customHome.customButtonHome} btn btn-lg`}
-                  style={{
-                    padding: "8px 70px",
-                  }}
-                  onClick={handleGenerateZPL}
-                >
-                  <strong>Submit</strong>
-                </button>
-              </div>
-              {labelImage && (
-                <div className="mt-4 text-center">
-                  <img src={labelImage} alt="Label" />
+                <div className="row mt-5">
+                  <div className="col-12 text-center">
+                    <h2
+                      className={`${customHome.customTextColorHeadings} display-4`}
+                    >
+                      Pack size
+                    </h2>
+                  </div>
                 </div>
-              )}
+
+                {/* Package options components */}
+                <div className="row justify-content-center mt-4 p-0 w-75 mx-auto">
+                  <div
+                    className={`${customHome.customColWidth} col-3 mx-4 d-flex justify-content-center mb-3`}
+                  >
+                    <PackageOption
+                      icon={
+                        <BoxFill
+                          className={`${customHome.customIcon} m-auto mt-5`}
+                        />
+                      }
+                      title="Small"
+                      sizeInfo="max. 10 x 40 x 65 cm"
+                      weightInfo="up to 10 kg"
+                      price="$15"
+                      value="Small"
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div
+                    className={`${customHome.customColWidth} col-3 mx-4 d-flex justify-content-center mb-3`}
+                  >
+                    <PackageOption
+                      icon={
+                        <BoxSeamFill
+                          className={`${customHome.customIcon} m-auto mt-5`}
+                        />
+                      }
+                      title="Medium"
+                      sizeInfo="max. 20 x 40 x 65 cm"
+                      weightInfo="up to 20 kg"
+                      price="$20"
+                      value="Medium"
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div
+                    className={`${customHome.customColWidth} col-3 mx-4 d-flex justify-content-center mb-3`}
+                  >
+                    <PackageOption
+                      icon={
+                        <Box2Fill
+                          className={`${customHome.customIcon} m-auto mt-5`}
+                        />
+                      }
+                      title="Large"
+                      sizeInfo="max. 45 x 40 x 65 cm"
+                      weightInfo="up to 30 kg"
+                      price="$25"
+                      value="Large"
+                      onChange={handleInput}
+                    />
+                  </div>
+                </div>
+
+                <div className="row mt-5">
+                  <div className="col-12 text-center">
+                    <h2
+                      className={`${customHome.customTextColorHeadings} display-4`}
+                    >
+                      Shipping details
+                    </h2>
+                  </div>
+                </div>
+
+                <div className="row justify-content-center mt-3 mb-5">
+                  <div className="container">
+                    <div className="row justify-content-center">
+                      <AddressFormSection
+                        label="Sender"
+                        zipCodeId="InputZipCode1"
+                        cityId="InputCity1"
+                        streetId="InputStreet1"
+                        buildingNumberId="InputBuildingNumber1"
+                        apartmentNumberId="InputApartmentNumber1"
+                        zipCode={values.InputZipCode1}
+                        city={values.InputCity1}
+                        street={values.InputStreet1}
+                        buildingNumber={values.InputBuildingNumber1}
+                        apartmentNumber={values.InputApartmentNumber1}
+                        errors={errors}
+                        handleInput={handleInput}
+                      />
+
+                      <AddressFormSection
+                        label="Recipient"
+                        zipCodeId="InputZipCode2"
+                        cityId="InputCity2"
+                        streetId="InputStreet2"
+                        buildingNumberId="InputBuildingNumber2"
+                        apartmentNumberId="InputApartmentNumber2"
+                        zipCode={values.InputZipCode2}
+                        city={values.InputCity2}
+                        street={values.InputStreet2}
+                        buildingNumber={values.InputBuildingNumber2}
+                        apartmentNumber={values.InputApartmentNumber2}
+                        errors={errors}
+                        handleInput={handleInput}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="text-center mt-2">
+                    <button
+                      type="submit"
+                      className={`${customHome.customButtonHome} btn btn-lg`}
+                      style={{
+                        padding: "8px 70px",
+                      }}
+                      onClick={handleGenerateZPL}
+                    >
+                      <strong>Submit</strong>
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </form>
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
